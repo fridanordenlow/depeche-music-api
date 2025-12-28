64356fa81b1f55f86083373f231f9ed8c7cba942
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-12-28 20:22:45 +0100

    feat: connect controller logic with auth middleware on spotify endpoints

--- src/routes/spotifyRouter.ts
+++ src/routes/spotifyRouter.ts
@@ -1,10 +1,29 @@
 import { Router } from 'express';
+import { attachAccessToken } from '../middlewares/spotifyAuthMiddleware.js';
+import {
+  fetchAlbum,
+  fetchArtist,
+  fetchArtistAlbums,
+  fetchNewReleases,
+  fetchTrack,
+} from '../controllers/spotifyController.js';
 
 export const spotifyRouter: Router = Router();
 
-spotifyRouter.get('/artists/:id');
-spotifyRouter.get('/artists/:id/albums');
-spotifyRouter.get('/albums/:id');
-spotifyRouter.get('/tracks/:id');
-spotifyRouter.get('/new-releases');
-// Add route for search
+spotifyRouter.get('/artists/:id', attachAccessToken, fetchArtist);
+spotifyRouter.get('/artists/:id/albums', attachAccessToken, fetchArtistAlbums);
+spotifyRouter.get('/albums/:id', attachAccessToken, fetchAlbum);
+spotifyRouter.get('/tracks/:id', attachAccessToken, fetchTrack);
+spotifyRouter.get('/new-releases', attachAccessToken, fetchNewReleases);
+// TODO - Add route for search
+
+// Hämta artistdetaljer - GET /api/spotify/artists/{id}
+// Hämta albumdetaljer - GET /api/spotify/albums/{id}
+// Hämta låtdetaljer - GET /api/spotify/tracks/{id}
+
+// Hämta albums låtar - GET /api/spotify/albums/{id}/tracks
+// Hämta artists album - GET /api/spotify/artists/{id}/albums
+
+// Generell sökning - GET /api/spotify/search
+// Landningssida/Nytt - GET /api/spotify/browse/new-releases
+// Populära låtar - GET /api/spotify/artists/{id}/top-tracks
