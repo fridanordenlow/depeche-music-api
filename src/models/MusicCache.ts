6d5550fa34a6dcf1f93bf370f8057ac67d6d9793
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-12-29 19:42:00 +0100

    feat: add MusicCache model for caching Spotify data

---
+++ src/models/MusicCache.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from 'mongoose';
+
+export interface IMusicCache extends Document {
+  spotifyId: string;
+  type: 'album' | 'artist' | 'track';
+  data: any; // Here I save my "cleaned" Spotify data
+  cachedAt: Date;
+}
+
+const MusicCacheSchema: Schema = new Schema({
+  spotifyId: { type: String, required: true, unique: true, index: true },
+  type: { type: String, enum: ['album', 'artist', 'track'], required: true },
+  data: { type: Schema.Types.Mixed, required: true },
+  cachedAt: { type: Date, default: Date.now, expires: 86400 }, // Cache expires after 24 hours
+});
+
+export default mongoose.model<IMusicCache>('MusicCache', MusicCacheSchema);
