2faefc0506f6df24855850eaac31a903c6f1d57b
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-02 13:12:26 +0100

    refactor: move register and login functionality to userController

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,42 +1,7 @@
-import { Response, Router } from 'express';
-import jwt from 'jsonwebtoken';
-import { AuthenticatedRequest } from '../types/express.types.js';
-import User from '../models/User.js';
+import { Router } from 'express';
+import { loginUser, registerUser } from '../controllers/userController.js';
 
 export const userRouter: Router = Router();
 
-userRouter.post('/register', async (req: AuthenticatedRequest, res: Response) => {
-  try {
-    const { username, email, password } = req.body;
-    const newUser = new User({
-      username,
-      email,
-      passwordHash: password,
-    });
-    const savedUser = await newUser.save();
-
-    res.status(201).json({
-      message: 'New user registered successfully!',
-      user: { id: savedUser._id, username: savedUser.username, email: savedUser.email },
-    });
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-userRouter.post('/login', async (req: AuthenticatedRequest, res: Response) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-
-    if (!user) return res.status(401).json({ error: 'Wrong email or password' });
-
-    const isPasswordMatch = await user.comparePassword(password);
-    if (!isPasswordMatch) return res.status(401).json({ error: 'Wrong email or password' });
-
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET as string, { expiresIn: '12h' });
-    res.json({ message: 'Login successful', token, user: { id: user._id, username: user.username } });
-  } catch (error: any) {
-    res.status(500).json({ error: 'Internal Server Error', details: error.message });
-  }
-});
+userRouter.post('/register', registerUser);
+userRouter.post('/login', loginUser);
