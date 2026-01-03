89e0247d2bc1c0de865b6dec7b5ad7f2abcbb40c
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-03 19:16:12 +0100

    chore: clean up code

--- src/utils/spotifyApiUtils.ts
+++ src/utils/spotifyApiUtils.ts
@@ -1,8 +1,7 @@
 import 'dotenv/config';
 import { RawImage } from '../types/spotify.raw.js';
 
-// const API_BASE_URL = process.env['SPOTIFY_API_BASE_URL'] as string;
-const API_BASE_URL = process.env.SPOTIFY_API_BASE_URL as string; // Does this work now?
+const API_BASE_URL = process.env.SPOTIFY_API_BASE_URL as string;
 
 export const fetchSpotifyData = async <T>(token: string, endpointPath: string): Promise<T> => {
   const spotifyUrl = `${API_BASE_URL}${endpointPath}`;
@@ -15,13 +14,14 @@ export const fetchSpotifyData = async <T>(token: string, endpointPath: string): 
       },
     });
 
+    // console.log(spotifyUrl);
     if (!response.ok) {
       const errorText = await response.text();
       const statusCode = response.status;
 
       const error = new Error(`Spotify API Error ${statusCode}`) as any;
       error.status = statusCode;
-      error.details = errorText; // Raw error details from Spotify API response for logging purposes
+      error.details = errorText;
 
       console.error(`Spotify API Error ${statusCode}: ${errorText}`);
       throw error;
