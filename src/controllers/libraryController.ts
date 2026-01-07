4f440fa375fdaa5d5cabfaea9e67db6a609483b8
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-07 12:32:06 +0100

    feat: add getUserLibrary function to retrieve user library

--- src/controllers/libraryController.ts
+++ src/controllers/libraryController.ts
@@ -2,6 +2,8 @@ import { Response } from 'express';
 import { AuthenticatedRequest } from '../types/express.types.js';
 import UserLibraryItem from '../models/UserLibraryItem.js';
 import { getMusicData } from '../services/musicCacheService.js';
+import MusicCache from '../models/MusicCache.js';
+import mongoose from 'mongoose';
 
 export const addToUserLibrary = async (req: AuthenticatedRequest, res: Response) => {
   try {
@@ -19,7 +21,7 @@ export const addToUserLibrary = async (req: AuthenticatedRequest, res: Response)
     await newItem.save();
 
     // Fetch metadata through the music cache service if needed
-    // It will eiither fetch from DB or Spotify API
+    // It will either fetch from DB or Spotify API
     const metadata = await getMusicData(spotifyItemId, itemType, token as string);
 
     res.status(201).json({
@@ -34,3 +36,43 @@ export const addToUserLibrary = async (req: AuthenticatedRequest, res: Response)
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
   }
 };
+
+export const getUserLibrary = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const userId = req.userId;
+
+    if (!userId) return res.status(400).json({ error: 'User not authenticated' });
+
+    const userObjectId = new mongoose.Types.ObjectId(userId);
+
+    // Items sorted descending, latest items first
+    const libraryItems = await UserLibraryItem.find({ userId: userObjectId }).sort({ addedAt: -1 });
+
+    // Search for metadata in MusicCache for each item
+    const libraryWithMetadata = await Promise.all(
+      libraryItems.map(async (item) => {
+        let metadata = await MusicCache.findOne({
+          spotifyId: item.spotifyItemId,
+          type: item.itemType,
+        });
+
+        if (!metadata) {
+          // If not found in cache, fetch and store it
+          const data = await getMusicData(item.spotifyItemId, item.itemType, req.spotifyToken as string);
+          return { ...item.toObject(), metadata: data };
+        }
+
+        return {
+          ...item.toObject(),
+          metadata: metadata.data,
+        };
+      })
+    );
+    res.status(200).json(libraryWithMetadata);
+  } catch (error: any) {
+    console.error('Library Error:', error);
+    res.status(500).json({ error: 'Internal Server Error', details: error.message });
+  }
+};
+
+// TODO : Implement other library management functions (remove, update status, etc.)
