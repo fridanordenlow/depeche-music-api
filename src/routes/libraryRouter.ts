b0a520672ff621131a6813025e7107b6ec5d38df
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-07 16:11:53 +0100

    feat: add getUserLibrary and removeUserLibraryItem routes

--- src/routes/libraryRouter.ts
+++ src/routes/libraryRouter.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { addToUserLibrary } from '../controllers/libraryController.js';
+import { addToUserLibrary, getUserLibrary, removeUserLibraryItem } from '../controllers/libraryController.js';
 import { userAuth } from '../middlewares/userAuth.js';
 import { attachAccessToken } from '../middlewares/spotifyAuth.js';
 
 export const libraryRouter: Router = Router();
 
 libraryRouter.post('/add', userAuth, attachAccessToken, addToUserLibrary);
+libraryRouter.get('/get', userAuth, attachAccessToken, getUserLibrary);
+libraryRouter.delete('/remove/:itemId', userAuth, removeUserLibraryItem);
+// Add update route later
