33904e79be0daf2362c297c76bc72ae804e75dd2
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-20 18:34:36 +0100

    refactor: restructure MusicCacheData to implement different properties for artist, album and track

--- src/types/music.types.ts
+++ src/types/music.types.ts
@@ -1,11 +1,40 @@
+// src/types/music.types.ts
 import { Track, Album, Artist } from './spotify.types.js';
 
-export interface MusicCacheData {
+interface BaseCache {
   spotifyId: string;
-  name: string;
-  type: 'track' | 'album' | 'artist';
   imageUrl: string;
-  artistName?: string;
   externalUrl: string;
-  fullData: Track | Album | Artist;
 }
+
+export interface ArtistCache extends BaseCache {
+  type: 'artist';
+  name: string;
+  fullData: Artist;
+}
+
+export interface AlbumCache extends BaseCache {
+  type: 'album';
+  title: string;
+  artists: any[];
+  fullData: Album;
+}
+
+export interface TrackCache extends BaseCache {
+  type: 'track';
+  title: string;
+  artists: any[];
+  fullData: Track;
+}
+
+export type MusicCacheData = ArtistCache | AlbumCache | TrackCache;
+
+// export interface MusicCacheData {
+//   spotifyId: string;
+//   name: string;
+//   type: 'track' | 'album' | 'artist';
+//   imageUrl: string;
+//   artistName?: string;
+//   externalUrl: string;
+//   fullData: Track | Album | Artist;
+// }
